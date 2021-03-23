4af93e71ee1a1fb6301f7b1c7c4a9f161ec3654d
Alex <alex123joha@gmail.com>
2021-03-23 15:21:10 -0500

    initial commit coffee app

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -4,12 +4,11 @@ const userModel = require('../models/user')
 const validateRole = async (role) => {
   const rolVal = await rolModel.findOne({ role });
   if (!rolVal) throw new Error(`El rol ${role} no existe en la BD`);
-};
+}
 
 const valUniqueEmail = async (email) => {
   const valEmail = await userModel.findOne( {email} );
   if ( valEmail ) throw new Error(`El correo ${ email } ya esta registrado`)
-  
 }
 
 const existUserById = async (id) => {
